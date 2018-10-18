aa7abb2b2c51d067ce43531bbe2bff07eee31cbc
Abraham Velazquez <abraham@dropbox.com>
2018-10-17 18:45:24 -0700

    get html to render out

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 $.getJSON('https://api.airtable.com/v0/appSrgke7E0ElZhMY/Locations?api_key=key2m8VgwGT2iztad',
-function(data){
-  console.log(data);
-});
+  function(airtable){
+    var html = [];
+    $.each(airtable.records, function(index, record) {
+      var name = record.fields['Name'];
+      var address = record.fields['Address'];
+      var rating = record.fields['Rating'];
+      html.push(`<h2>${name}, ${address}, ${rating}</h2>`);
+    });
+    $('body').append(html);
+  }
+);
