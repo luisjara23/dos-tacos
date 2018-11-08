0f17357d61803b68882d7f7759b2504d36c27f84
Abraham Velazquez <abraham@dropbox.com>
2018-11-07 18:16:14 -0800

    Add picture logic

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ $.getJSON('https://api.airtable.com/v0/appSrgke7E0ElZhMY/Locations?api_key=key2m
       var name = record.fields['Name'];
       var address = record.fields['Address'];
       var rating = record.fields['Rating'];
+      var picture = record.fields['Pictures'];
+      // console.log(picture);
+      if(picture[0]) {
+        html.push(`<img src="${picture[0].url}" />`)
+      }
       html.push(`<h2>${name}, ${address}, ${rating}</h2>`);
     });
     $('body').append(html);
